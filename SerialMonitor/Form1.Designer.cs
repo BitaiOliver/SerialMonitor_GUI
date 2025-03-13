873926dcf22a22bc1c38d48018fdf0a8dd71bc0e
BitaiOliver <olyver_bitai@yahoo.com>
2025-03-13 22:57:42 +0100

    Pushing complete project.

---
+++ SerialMonitor/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace SerialMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.cmbBaudrate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port:";
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(110, 12);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cmbPorts.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(337, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Location = new System.Drawing.Point(33, 98);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceive.Size = new System.Drawing.Size(379, 190);
+            this.txtReceive.TabIndex = 3;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(33, 322);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(296, 27);
+            this.txtSend.TabIndex = 4;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(418, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(244, 294);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 23);
+            this.btnReceive.TabIndex = 6;
+            this.btnReceive.Text = "Receive";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(244, 355);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // cmbBaudrate
+            // 
+            this.cmbBaudrate.FormattingEnabled = true;
+            this.cmbBaudrate.Location = new System.Drawing.Point(110, 39);
+            this.cmbBaudrate.Name = "cmbBaudrate";
+            this.cmbBaudrate.Size = new System.Drawing.Size(121, 21);
+            this.cmbBaudrate.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "BaudRate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "dT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(188, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ID";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(228, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Scope";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(299, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 392);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBaudrate);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cmbPorts);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.Button btnSend;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox cmbBaudrate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SerialMonitor/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialMonitor
+{
+    public partial class Form1 : Form
+    {
+        private int seq = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cmbPorts.Items.AddRange(ports);
+            //cmbPorts.SelectedIndex = 1;
+
+            cmbBaudrate.Items.Add("9600");
+
+            btnClose.Enabled = false;
+            btnSend.Enabled = false;
+            //btnReceive.Enabled = false;
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = false;
+            btnClose.Enabled = true;
+            btnSend.Enabled = true;
+            btnReceive.Enabled = true;
+
+            try
+            {
+                serialPort1.PortName = cmbPorts.Text;
+                serialPort1.BaudRate = Convert.ToInt32(cmbBaudrate.Text);
+                serialPort1.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+            btnSend.Enabled = false;
+            btnReceive.Enabled = false;
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine(txtSend.Text + Environment.NewLine);
+                    txtSend.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data = GetFormattedLogEntry();
+
+                txtReceive.Text +=  data;
+
+                if (serialPort1.IsOpen)
+                {
+                    // txtReceive.Text = serialPort1.ReadExisting();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+            //txtReceive.Text = serialPort1.ReadExisting();
+            SetText(serialPort1.ReadExisting());
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.txtReceive.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.txtReceive.Text += text;
+            }
+        }
+
+        /* Returns the current time in a string wich is exactly 16 char long*/
+        private string GetFormattedDate()
+        {
+            string date = " ";
+            string padding = "";
+            int temp = 0;
+            char[] cut = { 'A', 'M', 'P' };
+
+            DateTime dt = DateTime.Now;
+            date += dt.ToLongTimeString();
+            date = date.Trim(cut);
+            date += "." + dt.Millisecond.ToString();
+
+            temp = date.Length;
+            for(int i = 0; i < (16 - temp); i++)
+            {
+                padding += "  ";
+            }
+            return date + padding;
+        }
+        /* Returns the formatted period in string. Length = 14 */
+        private string GetFormattedPeriod(int period)
+        {
+            int digits = 1;
+            int temp = period;
+            string ret = "    ";
+            if(1000000 > period)
+            {
+                while (0 < (temp / 10))
+                {
+                    digits++;
+                    temp = temp / 10;
+                }
+                for (int i = 0; i < (6 - digits); i++)
+                {
+                    ret += " ";
+                }
+                ret += period.ToString();
+                ret += "    ";
+            }
+            else
+            {
+                ret = "    Error!    ";
+            }
+
+            return ret;
+        }
+        /* Returns the formatted ID in string. Length = 14 */
+        private string GetFormattedID(int id)
+        {
+            int digits = 1;
+            int temp = id;
+            string ret = "     ";
+
+            if (1000 > id)
+            {
+                while (0 < (temp / 10))
+                {
+                    digits++;
+                    temp = temp / 10;
+                }
+                for (int i = 0; i < (3 - digits); i++)
+                {
+                    ret += " ";
+                }
+                ret += id.ToString();
+                ret += "      ";
+            }
+            else
+            {
+                ret = "    Error!    ";
+            }
+            return ret;
+        }
+
+        /* Returns the formatted scope in string. Length = 18 */
+        private string GetFormattedScope(int scope)
+        {
+            string ret = "    ";
+
+            switch(scope)
+            {
+                case 1:
+                    {
+                        ret = "    Data    ";
+                        break;
+                    }
+                case 2:
+                    {
+                        ret = "  Response  ";
+                        break;
+                    }
+                default:
+                    {
+                        ret = "   Error!   ";
+                        break;
+                    }
+            }
+            ret += "  ";
+            return ret;
+        }
+        /* Returns the formatted data in string. Length = variable */
+        private string GetFormattedData(int data)
+        {
+            string ret = "      ";
+            ret += data.ToString();
+            return ret;
+        }
+        /* Returns the formatted data in string. Length = variable */
+        private string GetFormattedData(string data)
+        {
+            string ret = "      ";
+            ret += data;
+            return ret;
+        }
+        /* Returns the formatted data line. Length = variable */
+        private string GetFormattedLogEntry()
+        {
+            string ret = "";
+
+            switch (seq)
+            {
+                case 0:
+                    {
+                        ret = GetFormattedDate();
+                        ret += GetFormattedPeriod(1234);
+                        ret += GetFormattedID(555);
+                        ret += GetFormattedScope(1);
+                        ret += GetFormattedData(123456789);
+                        seq++;
+                        break;
+                    }
+                case 1:
+                    {
+                        ret = GetFormattedDate();
+                        ret += GetFormattedPeriod(134);
+                        ret += GetFormattedID(555);
+                        ret += GetFormattedScope(1);
+                        ret += GetFormattedData(123456789);
+                        seq++;
+                        break;
+                    }
+                case 2:
+                    {
+                        ret = GetFormattedDate();
+                        ret += GetFormattedPeriod(1234);
+                        ret += GetFormattedID(55);
+                        ret += GetFormattedScope(1);
+                        ret += GetFormattedData(123456789);
+                        seq++;
+                        break;
+                    }
+                case 3:
+                    {
+                        ret = GetFormattedDate();
+                        ret += GetFormattedPeriod(1234);
+                        ret += GetFormattedID(555);
+                        ret += GetFormattedScope(2);
+                        ret += GetFormattedData(123456789);
+                        seq = 0;
+                        break;
+                    }
+
+            }
+            /*
+            ret = GetFormattedDate();
+            ret += GetFormattedPeriod(1234);
+            ret += GetFormattedID(555);
+            ret += GetFormattedScope(1);
+            ret += GetFormattedData(123456789);*/
+            ret += "\r\n";
+            return ret;
+        }
+
+
+
+
+    }
+}
